1d4255894c1639079fe312f10d6e8d7b4539e4ee
石佳欢 <haut.1604.Stone@gmail.com>
2021-11-18 16:19:36 +0800

    更新了自动化数据生成流程

---
+++ tools/0_main.py
@@ -0,0 +1,5 @@
+from .DataDivider import DataDivider
+
+
+if __name__ == '__main__':
+    DataDivider.divide_rel_data(10)
---
+++ tools/DataDivider.py
@@ -0,0 +1,36 @@
+
+
+class DataDivider:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def divide_rel_data(wide):
+        data_file = open('模型用/relation.txt', encoding='utf-8')
+        dde_test_file = open('../data/dde/dde_test.txt', 'w', encoding='utf-8')
+        dde_train_file = open('../data/dde/dde_train.txt', 'w', encoding='utf-8')
+        dde_val_file = open('../data/dde/dde_val.txt', 'w', encoding='utf-8')
+
+        counter = 0
+
+        while True:
+            data = data_file.readline()
+            if not data:
+                break
+            counter = (counter + 1) % wide  # 8:1:1 划分
+            if counter < 8:
+                dde_train_file.write(data)
+            elif counter < 9:
+                dde_test_file.write(data)
+            else:
+                dde_val_file.write(data)
+
+        data_file.close()
+        dde_val_file.close()
+        dde_test_file.close()
+        dde_train_file.close()
+
+
+if __name__ == '__main__':
+    DataDivider.divide_rel_data(10)
